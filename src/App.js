52c5ade111790143fb74369fa60b0431f21f2d1f
wasif <wasif.aleem@braincrop.net>
2023-06-08 12:09:01 +0500

    basic socket

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 import { io } from "socket.io-client";
 
 function App() {
   const socket = useRef();
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]); // Use state hook to manage chat messages
 
   useEffect(() => {
-    socket.current = io("ws://localhost:9013");
+    socket.current = io("http://localhost:5000");
 
-    socket.current.on("connnection", () => {
+    socket.current.on("connection", () => {
       console.log("connected to server");
+    });
+
+    socket.current.on("message", (msg) => {
+      setChat((prevChat) => [...prevChat, msg]);
     });
   }, []);
 
-  const handleClick = () => {
-    socket.current.emit("message", new Date().getTime());
+  const send = () => {
+    socket.current.emit("message", message);
+    setMessage("")
   };
 
   return (
     <div className="App">
-      <p>Socket.io app</p>
+      <div className="chat-box">
+        <p>Socket.io app</p>
+        <input placeholder="..." value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button type="button" onClick={send}>
+          Send
+        </button>
 
-      <button type="button" onClick={handleClick}>
-        Emit a time message
-      </button>
+        <div>
+          {chat.map((msg, index) => (
+            <p key={index}>{msg}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
